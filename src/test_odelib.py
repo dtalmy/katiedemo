e8ab49548e21586e9fa02ed69f9804f4841d5a39
Donna 'Katie' McCullough <dmccull8@vols.utk.edu>
2021-09-30 02:31:34 -0400

    fixed NO str

---
+++ src/test_odelib.py
@@ -0,0 +1,54 @@
+import ODElib 
+import pandas as pd
+import numpy as np
+import pylab as pl
+
+
+# reading master dataset
+master_df = pd.read_csv('../data/tradeoffdat.csv')
+
+
+
+# select a random subset for plotting
+zerodmspdat = master_df[master_df['treatment']=='0DMSP']
+h0dmsp = zerodmspdat[zerodmspdat['control']==True]
+single= h0dmsp[h0dmsp['organism']=='H']
+
+
+
+# now plot
+f,ax = pl.subplots()
+ax.plot(single.time,single.r1,'-o')
+ax.plot(single.time,single.r2,'-o')
+ax.plot(single.time,single.r3,'-o')
+ax.set_xlabel('Time (days)')
+ax.set_ylabel('Abundance (ml-1)')
+
+
+
+# savefig
+f.savefig('../figures/demodat')
+pl.show()
+
+
+########################################################
+#
+########################################################
+
+
+########################################################
+#
+########################################################
+
+
+########################################################
+#
+########################################################
+
+
+########################################################
+#
+########################################################
+
+
+
